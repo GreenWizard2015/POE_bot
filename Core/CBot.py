c1e1913d180fcc3968e0b980e208fc48305878f5
GreenWizard <sizeof.2011@gmail.com>
2020-06-13 01:14:27 +0300

    Save screenshot when A pressed

--- Core/CBot.py
+++ Core/CBot.py
@@ -1,5 +1,8 @@
 import win32api
 from .CBotDebugState import CBotDebugState
+import cv2
+import time
+import os
 
 class CBot:
   def __init__(self, logger):
@@ -12,4 +15,15 @@ class CBot:
   def process(self, screenshot):
     actions = []
     debug = CBotDebugState(screenshot, self.logger)
+    
+    if (win32api.GetAsyncKeyState(ord('A')) & 1) == 1:
+      self.saveScreenshot(screenshot)
+      
     return (actions, debug)
+  
+  def saveScreenshot(self, screenshot):
+    os.makedirs("screenshots", exist_ok=True)
+    fname = 'screenshots/%d.jpg' % time.time_ns()
+    cv2.imwrite(fname, screenshot)
+    self.logger.info('Screenshot saved to %s' % fname)
+    return
